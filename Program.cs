2605ae334436b1fdd15fb93c20d612628074d2eb
Jayson Concepcion <ysonconcepcion@gmail.com>
2021-12-06 17:25:40 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Problem2
+{
+    static void Main(string[] args)
+    {
+        int N = 0;
+        int sum1 = 0;
+        int sum2 = 0;
+        N = Int32.Parse(Console.ReadLine().Trim());
+
+        for (int i = 0; i < N; i++)
+        {
+            string input = Console.ReadLine().Trim();
+            string[] row = input.Split(' ');
+            int[] arr = Array.ConvertAll(row, s => int.Parse(s));
+            sum1 += arr[i];
+            sum2 += arr[N-i-1];
+
+        }
+
+        Console.WriteLine(Math.Abs(sum1-sum2));
+
+    }
+}
